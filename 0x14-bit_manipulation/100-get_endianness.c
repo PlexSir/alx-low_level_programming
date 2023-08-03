3fdf5de91b5d55f1086b69cfeb022d778811974e
PlexSir <khayak3@gmail.com>
2023-08-03 16:53:18 -0700

    Basic-Programming

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 
 /**
  * get_endianness - determines the endianness of a machine (little or big)
- * Returns: 0 for big endian, 1 for little endian
+ * Return: 0 for big endian, 1 for little endian
  */
 int get_endianness(void)
 {
